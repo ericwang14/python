6f2c119172b86518cfe10565c68bff411235d21c
ericw <yeak2001@gmail.com>
2015-11-19 16:51:02 -0800

    admin site change

--- web/django-test/mysite/polls/admin.py
+++ web/django-test/mysite/polls/admin.py
@@ -1,3 +1,23 @@
+#! python
+# -*-encoding=utf-8 -*-
+#polls/admin.py
 from django.contrib import admin
 
+from .models import Question, Choice
 # Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,              {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('quesiton_text', 'pub_date', 'was_published_recently')
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice) # remove for now since we don't need separate page for choice
+
