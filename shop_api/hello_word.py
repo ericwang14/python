2c195dd0642d755fc0048dadb182db852cfb004f
ericwang14 <yeak2001@gmail.com>
2015-11-29 10:21:26 -0800

    shop APN checkin

---
+++ shop_api/hello_word.py
@@ -0,0 +1,11 @@
+from urllib2 import Request, urlopen
+from urllib import urlencode, quote_plus
+
+
+
+url = 'https://api.shop.com:8443/hello-world'
+headers = {  'apikey':'l7xx175ed35d49844df29b26917291a11038'  }
+request = Request(url, headers=headers)
+request.get_method = lambda: 'GET' 
+response_body = urlopen(request).read()
+print response_body
