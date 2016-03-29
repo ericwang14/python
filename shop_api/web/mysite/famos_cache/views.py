1e9438e6121da78049d7b996e45398e797301a4d
ericw <yeak2001@gmail.com>
2016-03-29 16:29:08 -0700

    add multiple thread support for famos cache clear request

--- shop_api/web/mysite/famos_cache/views.py
+++ shop_api/web/mysite/famos_cache/views.py
@@ -4,6 +4,7 @@
 # Create your views here.
 from django.shortcuts import render
 from famos_cache.models import AppServer, Site, Environment
+from concurrent import futures
 
 import requests
 
@@ -48,11 +49,34 @@ def clear(request):
                         clear_url = url % (app.name, env.host, env.service_url)
                         clear_url += "&key=" + key.strip()
                         clear_url += "&locale=" + site.locale.strip()
-                        requests.get(clear_url)
+                        # requests.get(clear_url)
                         clear_urls.append(clear_url)
+
+    with futures.ThreadPoolExecutor(50) as executor:
+        fs = [executor.map(request_to_clear, [clear_url]) for clear_url in clear_urls]
+        # futures.wait(fs)
 
     return render(request, 'famos_cache/done.html', {'clear_urls': clear_urls})
 
 
+def request_to_clear(clear_url):
+    print("calling: " + clear_url)
+    requests.get(clear_url)
+    print("calling end!\n")
 
-
+#
+# class myThread(threading.Thread):
+#     def __init__(self, thread_id, name, request_url):
+#         """
+#
+#         :rtype: object
+#         """
+#         threading.Thread.__init__(self)
+#         self.thread_id = thread_id
+#         self.name = name
+#         self.request_url = request_url
+#
+#     def run(self):
+#         print("thread: " + self.name + " calling: " + self.request_url)
+#         requests.get(self.request_url)
+#         print("thread: " + self.name + " calling end!\n")
